9926b9e30d1b29e410bdb87f72fc9bae7650acf2
MrTobar04 <tobabrego@gmail.com>
2026-09-09 21:15:23 -0600

    feat(tests): crear pruebas unitarias para Personas

--- AutoGestion/tests/AutoGestion.Tests/UnitTests/PersonasCrudTests.cs
+++ AutoGestion/tests/AutoGestion.Tests/UnitTests/PersonasCrudTests.cs
@@ -42,6 +42,27 @@ namespace AutoGestion.Tests.UnitTests
         }
 
         [Fact]
+        public async Task ObtenerPersonas_RetornaListadoCompletoDePersonas()
+        {
+            // Arrange
+            Context.Personas.AddRange(
+                new Persona { Nombre = "Persona Uno", DUI = "11111111-1" },
+                new Persona { Nombre = "Persona Dos", DUI = "22222222-2" }
+            );
+            await Context.SaveChangesAsync();
+
+            // Act
+            var result = await _controller.ObtenerPersonas();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, okResult.StatusCode);
+
+            var lista = Assert.IsAssignableFrom<IEnumerable<Persona>>(okResult.Value);
+            Assert.Equal(2, lista.Count());
+        }
+
+        [Fact]
         public async Task ObtenerPorId_IdExistente_RetornaOkConPersona()
         {
             // Arrange
@@ -76,6 +97,34 @@ namespace AutoGestion.Tests.UnitTests
         }
 
         [Fact]
+        public async Task ActualizarPersona_IdExistente_ActualizaExitosamenteYRetornaOk()
+        {
+            // Arrange
+            var persona = new Persona { Nombre = "Nombre Original", DUI = "12345678-9" };
+            Context.Personas.Add(persona);
+            await Context.SaveChangesAsync();
+
+            var personaModificada = new Persona
+            {
+                Id = persona.Id,
+                Nombre = "Nombre Actualizado",
+                DUI = "98765432-1"
+            };
+
+            // Act
+            var result = await _controller.ActualizarPersona(persona.Id, personaModificada);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, okResult.StatusCode);
+
+            var actualizadoEnDb = await Context.Personas.FindAsync(persona.Id);
+            Assert.NotNull(actualizadoEnDb);
+            Assert.Equal("Nombre Actualizado", actualizadoEnDb.Nombre);
+            Assert.Equal("98765432-1", actualizadoEnDb.DUI);
+        }
+
+        [Fact]
         public async Task ActualizarPersona_IdInexistente_RetornaNotFoundOBadRequest()
         {
             // Arrange
@@ -88,6 +137,39 @@ namespace AutoGestion.Tests.UnitTests
 
             // Act
             var result = await _controller.ActualizarPersona(88888, personaParaActualizar);
+
+            // Assert
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal(404, notFoundResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task EliminarPersona_IdExistente_EliminaExitosamenteYRetornaOk()
+        {
+            // Arrange
+            var persona = new Persona { Nombre = "Persona Para Eliminar", DUI = "55555555-5" };
+            Context.Personas.Add(persona);
+            await Context.SaveChangesAsync();
+
+            // Act
+            var result = await _controller.EliminarPersona(persona.Id);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, okResult.StatusCode);
+
+            var eliminadoDeDb = await Context.Personas.FindAsync(persona.Id);
+            Assert.Null(eliminadoDeDb);
+        }
+
+        [Fact]
+        public async Task EliminarPersona_IdInexistente_RetornaNotFoundOBadRequest()
+        {
+            // Arrange
+            int idInexistente = 77777;
+
+            // Act
+            var result = await _controller.EliminarPersona(idInexistente);
 
             // Assert
             var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
